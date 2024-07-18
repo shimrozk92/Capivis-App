0e54e6fe2956c4cb367f45dea98a11d0c8928d1b
Shimroz@1992 <shimrozk92@gmail.com>
2024-07-18 22:03:00 +0530

    resolve conflicts

--- db/migrate/20240710101740_add_fields_to_donor_questionnaire.rb
+++ db/migrate/20240710101740_add_fields_to_donor_questionnaire.rb
@@ -1,6 +1,5 @@
 class AddFieldsToDonorQuestionnaire < ActiveRecord::Migration[7.1]
   def change
     add_reference :donor_questionnaires, :donor_questionnaire_forms, foreign_key: true
-    remove_column :donor_questionnaires, :questions, :json
   end
 end
