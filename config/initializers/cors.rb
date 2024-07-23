9be8393bda185c20c686a09ffc165387fab9a443
Shimroz@1992 <shimrozk92@gmail.com>
2024-07-23 16:58:39 +0530

    varification key

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173", "203.161.58.56"
+    origins "localhost:5173", "203.161.58.56", "localhost:4000"
 
     resource "*",
       headers: :any,
